d8eb6ac73aefde43308af82e0ee96942048f642f
JD Wong <jdmswong@gmail.com>
2018-01-21 09:27:21 -0800

    scss automatically compiles, removed .idea files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// CSS
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
